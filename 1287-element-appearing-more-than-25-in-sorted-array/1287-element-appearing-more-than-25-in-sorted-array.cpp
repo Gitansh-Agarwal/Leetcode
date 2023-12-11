e3fb8b93ec82a1b5b8aee43ab0b37f04879ab085
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2023-12-11 18:25:44 +0530

    Time: 13 ms (42.19%), Space: 12.7 MB (92.26%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
@@ -2,36 +2,60 @@ class Solution {
 public:
     int findSpecialInteger(vector<int>& arr) {
         
+//         Below method Time Complexity = O(N) and Space Complexity = O(N).
+//         vector<int> prefixSum;
+//         int length = arr.size();
+//         int diff = arr[0];
+//         int diff2;
+//         int count = 1;
+//         prefixSum.push_back(arr[0]);
+//         for(int i=1; i<length; i++){
+            
+//             prefixSum.push_back(prefixSum[i-1] + arr[i]);
+            
+//         }
+//         for(int i=1; i<length; i++){
+            
+//             diff2 = prefixSum[i] - prefixSum[i-1];
+            
+//             if(diff2 == diff){
+//                 count++;
+//             }
+//             else{
+//                 diff = diff2;
+//                 count = 1;
+//             }
+            
+//             if(count > (length/4)){
+                
+//                 return diff;
+//             }
+            
+//         }
+//         return diff;
         
-        vector<int> prefixSum;
-        int length = arr.size();
-        int diff = arr[0];
-        int diff2;
+        
+        //Below Method Time Comlexity = O(N) and Space Complexity = O(1).
+        
         int count = 1;
-        prefixSum.push_back(arr[0]);
+        int length = arr.size();
+        int need = length/4;
+        
         for(int i=1; i<length; i++){
             
-            prefixSum.push_back(prefixSum[i-1] + arr[i]);
-            
-        }
-        for(int i=1; i<length; i++){
-            
-            diff2 = prefixSum[i] - prefixSum[i-1];
-            
-            if(diff2 == diff){
+            if(arr[i] == arr[i-1]){
                 count++;
             }
             else{
-                diff = diff2;
+                
+                if(count > need){
+                    
+                    return arr[i-1];
+                }
                 count = 1;
             }
-            
-            if(count > (length/4)){
-                
-                return diff;
-            }
-            
         }
-        return diff;
-    }
+        return arr[length -1];
+            
+   }
 };
