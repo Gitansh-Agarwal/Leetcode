84ccd527e43a04f1b5756109b9cf5af524f5b4ef
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2023-12-12 18:07:16 +0530

    Time: 3 ms (82.74%), Space: 10.3 MB (62.52%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -2,27 +2,51 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
      
-        int length = nums.size();
-        int max1 = INT_MIN;
-        int max2 = INT_MIN;
-        int index;
-        for(int i=0; i<length; i++){
+//         finding the maximum and the second maximum value by using two for loops so as to find the result 
+//         int length = nums.size();
+//         int max1 = INT_MIN;
+//         int max2 = INT_MIN;
+//         int index;
+//         for(int i=0; i<length; i++){
             
-            if(nums[i] > max1){
+//             if(nums[i] > max1){
                 
-                max1 = nums[i];
-                index = i;
+//                 max1 = nums[i];
+//                 index = i;
+//             }
+//         }
+        
+//         for(int i=0; i<length; i++){
+            
+//             if(nums[i] > max2 and i!=index){
+                
+//                 max2 = nums[i];
+//             }
+//         }
+        
+//         return (max1-1)*(max2-1);
+        
+        
+        // Another method to find the maximum and second maximum value but using only a single for loop so as to find the result.
+        
+        int firstMax = 0;
+        int secondMax = 0;
+        
+        // Using for each loop 
+        for(auto x : nums){
+            
+            if(x > firstMax){
+                
+                secondMax = firstMax;
+                firstMax = x;
+            }
+            else if(x > secondMax){
+                
+                secondMax = x;
             }
         }
         
-        for(int i=0; i<length; i++){
-            
-            if(nums[i] > max2 and i!=index){
-                
-                max2 = nums[i];
-            }
-        }
+        return (firstMax-1)*(secondMax-1);
         
-        return (max1-1)*(max2-1);
     }
 };
