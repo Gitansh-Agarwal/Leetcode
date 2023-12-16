8bf8cee5b51ad48843a98c850a396bbf4bf06828
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2023-12-16 15:32:36 +0530

    Time: 4 ms (87.26%), Space: 7.6 MB (79.21%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -5,21 +5,39 @@ public:
         if(s.size() != t.size()){
             return false;
         }
+//         using Hash Table
         
-        int *HT = new int[26];
-        for(int i=0; i<26; i++){
-            HT[i] = 0;
-        }
-        for(int i=0; s[i]!='\0'; i++){
-            HT[s[i]-97]++;
-        }
-        for(int i=0; t[i]!='\0'; i++){
-            HT[t[i]-97]--;
-        }
+//         int *HT = new int[26];
+//         for(int i=0; i<26; i++){
+//             HT[i] = 0;
+//         }
+//         for(int i=0; s[i]!='\0'; i++){
+//             HT[s[i]-97]++;
+//         }
+//         for(int i=0; t[i]!='\0'; i++){
+//             HT[t[i]-97]--;
+//         }
         
+//         for(int i=0; i<26; i++){
+//             if(HT[i] != 0){
+//                 return false; 
+//             }
+//         }
+//         return true;
+        
+        //Without using Hash Table(not allocating memory dynamically) and using for each loop.
+        
+        vector<int> vec(26,0);
+        
+        for(auto i : s){
+            vec[i-'a']++;
+        }
+        for(auto i : t){
+            vec[i-'a']--;
+        }
         for(int i=0; i<26; i++){
-            if(HT[i] != 0){
-                return false; 
+            if(vec[i]!=0){
+                return false;
             }
         }
         return true;
