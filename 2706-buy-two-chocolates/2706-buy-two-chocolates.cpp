f79034a7b3eddaecb1a7ee4057f1b3ba9a9c80ea
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2023-12-20 17:04:44 +0530

    Time: 23 ms (5.34%), Space: 47 MB (58.30%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -2,23 +2,32 @@ class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
         
-        int smallest = INT_MAX;
-        int secondSmallest = INT_MAX;
-        int length = prices.size();
-        int leftoverMoney;
-        for(int i=0; i<length; i++){
-            if(prices[i] < smallest){
-                secondSmallest = smallest;
-                smallest = prices[i];
-            }
-            else if(prices[i] < secondSmallest){
-                secondSmallest = prices[i];
-            }
-        }
-        leftoverMoney = money - (smallest + secondSmallest);
-        if(leftoverMoney >= 0){
+        // Below solution takes O(N) time.
+        // Intuition is to find the minimum and the second minimum element in the array and sum them as their sum will be minimum.
+        // int smallest = INT_MAX;
+        // int secondSmallest = INT_MAX;
+        // int length = prices.size();
+        // int leftoverMoney;
+        // for(int i=0; i<length; i++){
+        //     if(prices[i] < smallest){
+        //         secondSmallest = smallest;
+        //         smallest = prices[i];
+        //     }
+        //     else if(prices[i] < secondSmallest){
+        //         secondSmallest = prices[i];
+        //     }
+        // }
+        // leftoverMoney = money - (smallest + secondSmallest);
+        // if(leftoverMoney >= 0){
+        //     return leftoverMoney;
+        // }
+        // return money;
+        
+        //Below method uses sorting the two arrays and it takes O(Nlog(N)) time.
+        sort(prices.begin(),prices.end());
+        int leftoverMoney = money - (prices[0] + prices[1]);
+        if(leftoverMoney >= 0)
             return leftoverMoney;
-        }
         return money;
     }
 };
