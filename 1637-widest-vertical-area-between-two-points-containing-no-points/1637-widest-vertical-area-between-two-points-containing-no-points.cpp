436ce3884fa6191a44ae0c503372bcb16714e489
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2023-12-21 17:53:23 +0530

    Time: 233 ms (60.64%), Space: 66.6 MB (87.58%) - LeetHub

--- 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.cpp
+++ 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
     int maxWidthOfVerticalArea(vector<vector<int>>& points) {
-        sort(points.begin(),points.end(), [](const vector<int>& a, const vector<int>& b){
+        
+        //Sorting the given 2d vector points on the basis of 1st column of every row in ascending order(by default) or according to this question we are just sorting the entire 2d array points based on the x- coordinate of the different points.
+        
+        
+        // sort(points.begin(),points.end(), [](const vector<int>& a, const vector<int>& b){
+        //     return a[0] < b[0];
+        // });
+        
+        // OR
+        
+        sort(points.begin(),points.end(), [](vector<int>& a, vector<int>& b){
             return a[0] < b[0];
         });
         
