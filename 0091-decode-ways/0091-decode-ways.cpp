48bf9e5228c1d75636fda72100247abca3f0300f
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2023-12-25 19:56:20 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (65.44%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -44,7 +44,7 @@ public:
     
     //Optimized version of above method of Dynamic Programming and which follows bottom-up approach and it is an iterative method.
     int numDecodings(string s) {
-        if(s.size()==0 || s[0]=='0') return 0; 
+        if(s.size()==0 || s[0]=='0') return 0;
         vector<int>dp(s.size()+1);
         dp[0]=1;
         dp[1]=1;
@@ -59,4 +59,39 @@ public:
         }
         return dp[n];
     }
+    
+    //Further optimizing the above method to use O(1) space instead of O(N) space.
+    
+    // The below code doesnot works properly so it needs to be modified.
+//     int numDecodings(string s) {
+//         if(s.size()==0 || s[0]=='0') return 0;
+//         int current;
+//         int prev = 1;
+//         int prevPrev = 1;
+//         // vector<int>dp(s.size()+1);
+//         // dp[0]=1;
+//         // dp[1]=1;
+//         int n=s.size();
+//         for(int i=2; i<=n; i++){
+//             if(s[i-1]=='0')
+//                 return prev;
+//             if(s[i-1]>='1' && s[i-1]<='9'){
+//                 current = prev;
+//                 // dp[i]=dp[i-1];
+//             }
+//             if(s[i-2]=='1'){
+//                 current+=prevPrev;
+//                 // dp[i]+=dp[i-2];
+//             }
+//             else if(s[i-2]=='2' && (s[i-1]>='0' && s[i-1]<='6')){
+//                 current+=prevPrev;
+//                 // dp[i]+=dp[i-2];
+//             }
+            
+//             prevPrev = prev;
+//             prev = current;
+//         }
+//         // return dp[n];
+//         return prev;
+//     }
 };
