0c8ace9a9a02d14ce4bc90997311966dbd40644f
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2023-12-27 15:31:52 +0530

    Time: 110 ms (81.67%), Space: 95.8 MB (37.26%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,23 +1,47 @@
 class Solution {
 public:
-    int minCost(string colors, vector<int>& neededTime) {
-        int minTime = 0;
+//     int minCost(string colors, vector<int>& neededTime) {
+//         int minTime = 0;
+//         int prev = 0;
+//         for(int i=1; colors[i]!='\0'; i++){
+//             if(colors[i]==colors[prev]){
+//                 if(neededTime[prev]<neededTime[i]){
+//                     minTime+=neededTime[prev];
+//                     prev=i;
+//                 }
+//                 else{
+//                     minTime+=neededTime[i];
+//                 }
+                
+//             }
+//             else{
+//                 prev=i;
+//             }
+//         }
+//         return minTime;
+//     }
+    
+    
+    // OR
+    
+    
+     int minCost(string colors, vector<int>& neededTime) {
+        int ans = 0;
         int prev = 0;
         for(int i=1; colors[i]!='\0'; i++){
-            if(colors[i]==colors[prev]){
+            if(colors[i]!=colors[prev]){
+                prev=i;
+            }
+            else{
                 if(neededTime[prev]<neededTime[i]){
-                    minTime+=neededTime[prev];
+                    ans+=neededTime[prev];
                     prev=i;
                 }
                 else{
-                    minTime+=neededTime[i];
+                    ans+=neededTime[i];
                 }
-                
-            }
-            else{
-                prev=i;
             }
         }
-        return minTime;
+        return ans;
     }
 };
