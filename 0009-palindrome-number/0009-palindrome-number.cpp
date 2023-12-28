9011bcb919f22f63d5571f5727e07fea3e797142
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2023-12-29 02:44:02 +0530

    Time: 27 ms (6.13%), Space: 6.3 MB (75.34%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)
+            return false;
+        int rev = 0;
+        int n=x;
+        int digit;
+        int max = 2147483647;//INT32_MAX.
+        int min = -2147483648;//INT32_MIN.
+        while(n!=0){
+            digit = n%10;
+            n/=10;
+            
+            if((rev > max/10) or (rev == max/10 && digit > max%10))
+                return false;
+                
+              
+            rev = rev*10 + digit;
+        }
+        
+        if(rev == x){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
