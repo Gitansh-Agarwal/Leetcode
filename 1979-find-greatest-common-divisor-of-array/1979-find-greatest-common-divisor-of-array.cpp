10f387ed5d5ad1d6fa2f11af793c57748fdab6f2
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2023-12-30 01:08:23 +0530

    Time: 12 ms (7.93%), Space: 12.8 MB (60.76%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
@@ -1,10 +1,24 @@
 class Solution {
 public:
     int findGCD(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n = nums[0];
-        int m = nums[nums.size()-1];
+        // sort(nums.begin(),nums.end());
+        // int n = nums[0];
+        // int m = nums[nums.size()-1];
         
+        // OR
+        
+        int m = INT_MIN;
+        int n = INT_MAX;
+        int length = nums.size();
+        for(int i=0; i<length; i++){
+            if(nums[i] < n)
+                n = nums[i];
+            if(nums[i] > m)
+                m = nums[i];
+        }
+        //m is the maximum value in the vector.
+        //n is the minimum value in the vector.
+        //Using Euclidean Algorithm
         while(n>0 && m>0){
             if(n>m)
                 n=n%m;
