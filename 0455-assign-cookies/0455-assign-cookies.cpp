777ac7b9e2c76b17789a5eec7fe6aaad774e5ce6
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-01-01 14:46:55 +0530

    Time: 46 ms (8.33%), Space: 17.9 MB (36.11%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -2,46 +2,46 @@ class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         //Ist Method 
-//         int lengthS = s.size();
-//         if(lengthS==0)
-//             return 0;
-//         sort(g.begin(), g.end());
-//         sort(s.begin(), s.end());
-//         int count = 0;
-//         int lengthG = g.size();
-//         int i=0, j=0; 
-//         while(i<lengthG && j<lengthS){
-//             if(g[i] <= s[j]){
-//                 count++;
-//                 i++;
-//                 j++;
-//             }
-//             else if(g[i] > s[j])
-//                 j++;
+        int lengthS = s.size();
+        if(lengthS==0)
+            return 0;
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int count = 0;
+        int lengthG = g.size();
+        int i=0, j=0; 
+        while(i<lengthG && j<lengthS){
+            if(g[i] <= s[j]){
+                count++;
+                i++;
+                j++;
+            }
+            else
+                j++;
                 
-//         }
-//         return count;
+        }
+        return count;
         
         // OR
         
         // IInd Method
-        sort(g.begin(), g.end(), greater<int>());
-        sort(s.begin(), s.end(), greater<int>());
+//         sort(g.begin(), g.end(), greater<int>());
+//         sort(s.begin(), s.end(), greater<int>());
         
-        int ans = 0;
-        int child = 0;
-        int cookie = 0;
+//         int ans = 0;
+//         int child = 0;
+//         int cookie = 0;
         
-        while(cookie < s.size() && child < g.size()){
-            if(g[child] <= s[cookie]){
-                ans++;
-                child++;
-                cookie++;
-            }
-            else{
-                child++;
-            }
-        }
-        return ans;
+//         while(cookie < s.size() && child < g.size()){
+//             if(g[child] <= s[cookie]){
+//                 ans++;
+//                 child++;
+//                 cookie++;
+//             }
+//             else{
+//                 child++;
+//             }
+//         }
+//         return ans;
     }
 };
