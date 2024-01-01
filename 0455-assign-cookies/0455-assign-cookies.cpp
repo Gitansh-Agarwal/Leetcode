0209cbd529c11a5e9a1e65f8316f2fda6112eff5
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-01-01 14:44:37 +0530

    Time: 20 ms (78.06%), Space: 17.9 MB (36.11%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,24 +1,47 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int lengthS = s.size();
-        if(lengthS==0)
-            return 0;
-        sort(g.begin(), g.end());
-        sort(s.begin(), s.end());
-        int count = 0;
-        int lengthG = g.size();
-        int i=0, j=0; 
-        while(i<lengthG && j<lengthS){
-            if(g[i] <= s[j]){
-                count++;
-                i++;
-                j++;
-            }
-            else if(g[i] > s[j])
-                j++;
+        //Ist Method 
+//         int lengthS = s.size();
+//         if(lengthS==0)
+//             return 0;
+//         sort(g.begin(), g.end());
+//         sort(s.begin(), s.end());
+//         int count = 0;
+//         int lengthG = g.size();
+//         int i=0, j=0; 
+//         while(i<lengthG && j<lengthS){
+//             if(g[i] <= s[j]){
+//                 count++;
+//                 i++;
+//                 j++;
+//             }
+//             else if(g[i] > s[j])
+//                 j++;
                 
+//         }
+//         return count;
+        
+        // OR
+        
+        // IInd Method
+        sort(g.begin(), g.end(), greater<int>());
+        sort(s.begin(), s.end(), greater<int>());
+        
+        int ans = 0;
+        int child = 0;
+        int cookie = 0;
+        
+        while(cookie < s.size() && child < g.size()){
+            if(g[child] <= s[cookie]){
+                ans++;
+                child++;
+                cookie++;
+            }
+            else{
+                child++;
+            }
         }
-        return count;
+        return ans;
     }
 };
