d98ac75f743b0cea99503d514887053ba2c48007
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-01-04 17:10:39 +0530

    Time: 114 ms (79.01%), Space: 85.1 MB (63.17%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -1,29 +1,58 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
+        
+        // unordered_map<int, int> mp;
+        // for(int i=0; i<nums.size(); i++){
+        //     ++mp[nums[i]];
+        // }
+        // int ans = 0;
+        // int countDivideBy3;
+        // for(auto it : mp){
+        //     if(it.second == 1)
+        //         return -1;
+        //     if(it.second%3 == 0){
+        //         ans+= it.second/3;
+        //     }
+        //     else if(it.second%3 == 2){
+        //         countDivideBy3 = it.second/3;
+        //         ans+=countDivideBy3;
+        //         it.second-=(countDivideBy3) * 3;
+        //         ans+=it.second/2;
+        //     }
+        //     else{
+        //         countDivideBy3 = (it.second/3) - 1;
+        //         ans+=countDivideBy3;
+        //         it.second-=(countDivideBy3)*3;
+        //         ans+=it.second/2;
+        //     }
+        // }
+        // return ans;
+        
+
+        // OR
+        
+        
         unordered_map<int, int> mp;
+        int ans = 0;
+        
         for(int i=0; i<nums.size(); i++){
             ++mp[nums[i]];
         }
-        int ans = 0;
-        int countDivideBy3;
-        for(auto it : mp){
-            if(it.second == 1)
-                return -1;
-            if(it.second%3 == 0){
-                ans+= it.second/3;
+        
+        for(auto i : mp){
+            if(i.second % 3 == 0){
+                ans += i.second/3;
             }
-            else if(it.second%3 == 2){
-                countDivideBy3 = it.second/3;
-                ans+=countDivideBy3;
-                it.second-=(countDivideBy3) * 3;
-                ans+=it.second/2;
+            else if(i.second % 3 == 2){
+                ans++;
+                ans += (i.second - 2) / 3;
             }
             else{
-                countDivideBy3 = (it.second/3) - 1;
-                ans+=countDivideBy3;
-                it.second-=(countDivideBy3)*3;
-                ans+=it.second/2;
+                if(i.second == 1)
+                    return -1;
+                ans+=2;
+                ans += (i.second - 4) / 3;
             }
         }
         return ans;
