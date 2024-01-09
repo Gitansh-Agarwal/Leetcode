1ec437b64134baf6b0cf88d05474f7af50f318a7
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-01-10 02:32:48 +0530

    Time: 0 ms (100.00%), Space: 13.3 MB (30.46%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -52,12 +52,43 @@ public:
         return vec1;
     }
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int> vec1;
-    vector<int> vec2;
-        vec1= preorder(root1, vec1);
-        vec2 = preorder(root2, vec2);
-        if(vec1 == vec2)
-            return true;
-        return false;
+        // vector<int> vec1;
+        // vector<int> vec2;
+        // vec1= preorder(root1, vec1);
+        // vec2 = preorder(root2, vec2);
+        // if(vec1 == vec2)
+        //     return true;
+        // return false;
+        
+        //OR
+        //Using deque or simply using queue
+        deque<int> leaves;
+        bool ans = 1;
+        
+        function<void(TreeNode*, bool)> traverse = [&](TreeNode* node, bool push){
+            if(!node)
+                return;
+            
+            if(!(node->left) && !(node->right)){
+                if(push){
+                    leaves.push_back(node->val);
+                }
+                else{
+                    if(leaves.empty() or node->val != leaves.front())
+                        ans = 0;
+                    
+                    if(!leaves.empty())
+                        leaves.pop_front();
+                }
+            }
+            
+            traverse(node->left, push);
+            traverse(node->right, push);
+        };
+        
+        traverse(root1, 1);
+        traverse(root2, 0);
+        
+        return (ans and leaves.empty());
     }
 };
