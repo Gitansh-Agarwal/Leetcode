d467de7122e477c3a5b14bd260ac1f83c8aa39f4
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-01-09 15:36:42 +0530

    Time: 0 ms (100.00%), Space: 17.9 MB (5.04%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -13,22 +13,39 @@ class Solution {
 public:
     
     vector<int> preorder(TreeNode* root1, vector<int>& vec1){
-        if(!root1)
-            return vec1;
-        if(!root1->left && !root1->right){
-            vec1.push_back(root1->val);
-            return vec1; 
-        }
-        if(root1->left != nullptr){
-            preorder(root1->left, vec1);
-        }
-        if(root1->right != nullptr){
-            preorder(root1->right, vec1);
-        }
+        // if(!root1)
+        //     return vec1;
+        // if(!root1->left && !root1->right){
+        //     vec1.push_back(root1->val);
+        //     return vec1; 
+        // }
+        // if(!root1->left){
+        //     preorder(root1->right, vec1);
+        // }
+        // if(!root1->right){
+        //     preorder(root1->left, vec1);
+        // }
         // else{
         //     preorder(root1->left, vec1);
         //     preorder(root1->right, vec1);
         // }
+        // return vec1;
+         if(!root1)
+            return vec1;
+        if(!root1->left && !root1->right){
+           vec1.push_back(root1->val);
+            return vec1;
+        }
+        if(!root1->left){
+            preorder(root1->right, vec1);
+        }
+        else if(!root1->right){
+            preorder(root1->left, vec1);
+        }
+        else{
+            preorder(root1->left, vec1);
+            preorder(root1->right, vec1);
+        }
         return vec1;
     }
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
