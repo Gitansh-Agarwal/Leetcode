fdf0f8bc2d7817bc563b0884cb54e1cd12e547e7
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-01-16 19:51:39 +0530

    Time: 177 ms (53.38%), Space: 97.5 MB (45.71%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -1,26 +1,63 @@
 class RandomizedSet {
 public:
+    //1st Method
+    // unordered_map<int, int> pos;
+    
+    //2nd Method
     unordered_map<int, int> pos;
+    vector<int> values;
+    
+    
     RandomizedSet() {
         
     }
     
     bool insert(int val) {
-        if(pos.find(val)!=pos.end())
+        
+        //1st Method
+        // if(pos.find(val)!=pos.end())
+        //     return 0;
+        // pos[val] = 1;
+        // return 1;
+        
+        
+        //2nd Method
+        if(pos.find(val) != pos.end())
             return 0;
-        pos[val] = 1;
+        pos[val] = values.size();
+        values.push_back(val);
         return 1;
     }
     
     bool remove(int val) {
-        if(pos.find(val)==pos.end())
+        //1st Method
+        // if(pos.find(val)==pos.end())
+        //     return 0;
+        // pos.erase(val);
+        // return 1;
+        
+        
+        //2nd Method
+        if(pos.find(val) == pos.end())
             return 0;
+        
+        int p1 = pos[val];
+        int p2 = values.size() - 1;
+        
+        swap(values[p1], values[p2]);
+        pos[values[p1]] = p1;
         pos.erase(val);
+        values.pop_back();
         return 1;
     }
     
     int getRandom() {
-        return next(pos.begin(), rand() % pos.size()) -> first;
+        //1st Method
+        // return next(pos.begin(), rand() % pos.size()) -> first;
+        
+        
+        //2nd Method
+        return values[rand() % values.size()];
     }
 };
 
