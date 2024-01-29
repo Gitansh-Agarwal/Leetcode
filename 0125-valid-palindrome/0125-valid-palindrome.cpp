98f445ced4d0099f2b652e44435f5ff36f07a10a
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-01-29 16:38:25 +0530

    Time: 4 ms (80.45%), Space: 9.3 MB (5.91%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string temp = "";
+        string tempReverse;
+        for(int i=0; s[i]!='\0'; i++){
+            if((s[i]>=48 && s[i]<=57) || (s[i]>=65 && s[i]<=90) || (s[i]>=97 && s[i]<=122)){
+                if(s[i]>=65 && s[i]<=90){
+                    temp.push_back((char)(s[i]+32));
+                }
+                else{
+                    temp.push_back(s[i]);
+                }
+            }
+        }
+        tempReverse = temp;
+        reverse(temp.begin(), temp.end());
+        if(temp == tempReverse)
+            return true;
+        return false;
+    }
+};
