38f64c50459b3fc6d7b3ec9bfadd60067141f00d
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-02-13 19:50:47 +0530

    Time: 58 ms (17.75%), Space: 29.7 MB (13.47%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
@@ -1,9 +1,23 @@
 class Solution {
 public:
     int isPalindrome(string reverseWord){
-        string word = reverseWord;
-        reverse(reverseWord.begin(), reverseWord.end());
-        if(word == reverseWord)
+        // string word = reverseWord;
+        // reverse(reverseWord.begin(), reverseWord.end());
+        // if(word == reverseWord)
+        //     return 1;
+        // return 0;
+        
+        // OR
+        
+        bool ok = 1;
+        int n = reverseWord.size(); 
+        for(int i=0; i<n/2; i++){
+            if(reverseWord[i] != reverseWord[n-i-1]){
+                ok = 0;
+                break;
+            }
+        }
+        if(ok)
             return 1;
         return 0;
     }
