1505f120668fcec0c60850e0d1cce34bccc572bb
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-02-22 13:30:37 +0530

    Time: 0 ms (100.00%), Space: 14.6 MB (33.39%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        //Brute Force Solution
+        //1. Generate all permutations in sorted order.
+        //2. Linear Search to find the given permutation.
+        //3. The permutation at the next index will be the required answer.
+        
+        
+        //Better Solution
+        // next_permutation(nums.begin(), nums.end());
+        
+        
+        //Optimal Solution
+        int index=-1;
+        int n=nums.size();
+        for(int i=n-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                index = i;
+                break;
+            }
+        }
+        if(index == -1){
+            reverse(nums.begin(), nums.end());
+        }
+        else{
+            for(int i=n-1; i>index; i--){
+                if(nums[i] > nums[index]){
+                    swap(nums[index], nums[i]);
+                    break;
+                }
+            }
+            reverse(nums.begin()+index+1, nums.end());
+        }
     }
 };
