7595dd1be65d107fcbbcc6e8d35d473597fe9810
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-02-24 19:20:52 +0530

    Time: 4 ms (39.32%), Space: 8.5 MB (59.99%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,18 +2,36 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         //Brute Force Solution
+        //But the below method allocates a 2d answer matrix which is wrong wrt to the question.
         //TC=O(N^2), extra SC=O(N^2).
+        // int n= matrix.size();
+        // vector<vector<int>> ans(n, vector<int>(n));
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<n; j++){
+        //         ans[j][n-1-i] = matrix[i][j];
+        //     }
+        // }
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<n; j++){
+        //         matrix[i][j] = ans[i][j];
+        //     }
+        // }
+        
+        
+        //Optimal Solution
+        //TC=O(N/2 * N/2){transpose} + O(N * N/2){reverse matrix rows}.
+        //Transpose Matrix = TC=O(N/2 * N/2){transpose}.
         int n= matrix.size();
-        vector<vector<int>> ans(n, vector<int>(n));
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                ans[j][n-1-i] = matrix[i][j];
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
+        
+        //Reverse Transposed Matrix Rows = TC=O(N * N/2){reverse matrix rows}.
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                matrix[i][j] = ans[i][j];
-            }
+            // row is matrix[i]
+            reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
