22eac363dfdc8a9af131b0117cc8a5923e750f9d
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-02-25 17:38:17 +0530

    Time: 87 ms (43.33%), Space: 50.5 MB (19.38%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        // Below method is giving TLE Error.
-        int sum;
-        int length = nums.size();
-        int count=0;
-        for(int sp=0; sp<length; sp++){
+        // Below method is working now somehow and is not giving TLE Error.
+//         int sum;
+//         int length = nums.size();
+//         int count=0;
+//         for(int sp=0; sp<length; sp++){
             
-            sum = 0;
-            for(int ep=sp; ep<length; ep++){
+//             sum = 0;
+//             for(int ep=sp; ep<length; ep++){
                 
-                sum+=nums[ep];
+//                 sum+=nums[ep];
                 
-                if(sum == k){
-                    count++;
-                }
-            }
-        }
-        return count;
+//                 if(sum == k){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
         
+        
+        
+        //Optimal Solution = Using Prefix Sum
+        //TC=O(N * logN){ordered map} or O(N){unordered map} 
+        //Extra SC=O(N).
+        int cnt=0;
+        unordered_map<int, int> mpp;
+        mpp[0] = 1;
+        int prefixSum=0;
+        for(int i=0; i<nums.size(); i++){
+            prefixSum+=nums[i];
+            int remove = prefixSum - k;
+            cnt+=mpp[remove];
+            mpp[prefixSum]+=1;
+        }
+        return cnt;
     }
 };
