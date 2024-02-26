cdbdbbcedec11b9751b6569f5b41cfb4e4029f41
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-02-26 17:31:40 +0530

    Time: 0 ms (100.00%), Space: 8 MB (13.41%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    //any value in the pascal's triangle can be computed using (n-1)C(r-1).
+    // here n is row and r is column.
+    //finding the ncr value using O(r) time.
+    //TC=O(r), extra SC=O(1).
+    int nCr(int n, int r){
+        long long res=1;
+        for(int i=0; i<r; i++){
+            res = res*(n-i);
+            res = res/(i+1);
+        }
+        return res;
+    }
+    //printing nth row of the pascal's triangle.
+    //TC=O(N*r), SC=O(N) , extra SC=O(1).
+    vector<int> generateRow(int n){
+        vector<int> temp;
+        for(int col=1; col<=n; col++){
+            temp.push_back(nCr(n-1, col-1));
+        }
+        return temp;
+    }
+    // or 
+    //TC=O(N){elements to be printed are equal to the row number}, SC=O(N){for storing the                 result},extra SC=O(1)
+    vector<int> generateRow1(int row){
+        vector<int> temp;
+        temp.push_back(1);
+        long long res=1;
+        for(int col=1; col<row; col++){
+            res=res*(row-col);
+            res=res/col;
+            temp.push_back(res);
+        }
+        return temp;
+    }
+    
+    vector<vector<int>> generate(int numRows) {
+        //Brute Force Solution
+        vector<vector<int>> ans;
+        for(int row=1; row<=numRows; row++){
+            vector<int> temp;
+            for(int col=1; col<=row; col++){
+                temp.push_back(nCr(row-1, col-1));
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
