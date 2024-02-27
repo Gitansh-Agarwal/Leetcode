7b7d02db0cbf6889bb6e718fa6850c0d77895df4
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-02-27 14:35:46 +0530

    Time: 13 ms (25.60%), Space: 19.5 MB (12.91%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,13 +3,17 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         unordered_map<int, int> mpp;
         vector<int> ans;
+        int n=nums.size();
+        int req=n/3+1;
         for(auto it: nums){
+            if(mpp[it] == req-1)
+                ans.push_back(it);
             mpp[it]++;
         }
-        for(auto it: mpp){
-            if(it.second > (nums.size()/3))
-                ans.push_back(it.first);
-        }
+        // for(auto it: mpp){
+        //     if(it.second > (nums.size()/3))
+        //         ans.push_back(it.first);
+        // }
         return ans;
     }
 };
