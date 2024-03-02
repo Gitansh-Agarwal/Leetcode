6078f2e88f6abde55703c25aa93e08bf862de81e
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-03-02 22:36:01 +0530

    Time: 29 ms (33.34%), Space: 28.3 MB (58.32%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,11 +1,34 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
+        //BRute Force Approach
+        //TC=O(NlogN) , extra SC=O(1).
+        // int n=nums.size();
+        // for(int i=0; i<n; i++){
+        //     nums[i]*= nums[i];
+        // }
+        // sort(nums.begin(), nums.end());
+        // return nums;
+        
+        
+        //Optimal Approach
+        //TC=O(N), SC=O(N){for returning the answer.}
         int n=nums.size();
-        for(int i=0; i<n; i++){
-            nums[i]*= nums[i];
+        vector<int> res(n);
+        int i=0, j=n-1;
+        int k=n-1;
+        while(i<=j){
+            int n1=nums[i]*nums[i];
+            int n2=nums[j]*nums[j];
+            if(n2 > n1){
+                res[k--] = n2;
+                j--;
+            }
+            else{
+                res[k--] = n1;
+                i++;
+            }
         }
-        sort(nums.begin(), nums.end());
-        return nums;
+        return res;
     }
 };
