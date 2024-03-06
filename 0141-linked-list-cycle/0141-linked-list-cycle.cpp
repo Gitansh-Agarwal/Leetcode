9141fbfc97bdfd46a74432d2585d2d89f42f64f1
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-03-07 01:30:32 +0530

    Time: 8 ms (59.82%), Space: 10.5 MB (76.02%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,18 +9,39 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head==nullptr || head->next==nullptr){
+        //TC=O(N),
+        //SC=O(N).
+        // if(head==nullptr || head->next==nullptr){
+        //     return false;
+        // }
+        // ListNode *tempHead=head;
+        // unordered_set<ListNode*> st;
+        // while(tempHead!=nullptr && st.find(tempHead)==st.end()){
+        //     st.insert(tempHead);
+        //     tempHead=tempHead->next;
+        // }
+        // if(tempHead==nullptr){
+        //     return false;
+        // }
+        // return true;
+        
+        
+        
+        //Optimal Solution= Floyd's Tortoise and Hare Algorithm.
+        //TC=O(N)
+        //SC=O(1).
+        if(head==nullptr || head->next==nullptr)
             return false;
-        }
-        ListNode *tempHead=head;
-        unordered_set<ListNode*> st;
-        while(tempHead!=nullptr && st.find(tempHead)==st.end()){
-            st.insert(tempHead);
-            tempHead=tempHead->next;
-        }
-        if(tempHead==nullptr){
-            return false;
-        }
-        return true;
+        ListNode *slow=head;
+        ListNode *fast=head;
+        do{
+            slow=slow->next;
+            fast=fast->next;
+            if(fast)
+                fast=fast->next;
+        }while(slow!=fast && fast!=nullptr);
+        if(slow==fast)
+            return true;
+        return false;
     }
 };
