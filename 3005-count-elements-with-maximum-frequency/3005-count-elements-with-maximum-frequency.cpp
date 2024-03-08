65e9161c411baae48b3a19db2f8228d5caf101e4
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-03-09 00:25:25 +0530

    Time: 5 ms (47.79%), Space: 22.2 MB (13.39%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
@@ -4,20 +4,35 @@ public:
         return a.second>b.second;
     }
     int maxFrequencyElements(vector<int>& nums) {
-        unordered_map<int, int> mpp;
+        // unordered_map<int, int> mpp;
+        // for(auto it:nums){
+        //     mpp[it]++;
+        // }
+        // vector<pair<int, int>> v(mpp.begin(), mpp.end());
+        // sort(v.begin(), v.end(), compare);
+        // // int maxFreq=(*(v.begin())).second;
+        // int maxFreq=v.begin()->second;
+        // int ans=0;
+        // for(auto it: v){
+        //     if(it.second==maxFreq)
+        //         ans+=it.second;
+        //     else
+        //         break;
+        // }
+        // return ans;
+        
+        
+        
+        vector<int> freq(101, 0);
+        int maxFreq=0;
         for(auto it:nums){
-            mpp[it]++;
+            freq[it]++;
+            maxFreq=max(maxFreq, freq[it]);
         }
-        vector<pair<int, int>> v(mpp.begin(), mpp.end());
-        sort(v.begin(), v.end(), compare);
-        // int maxFreq=(*(v.begin())).second;
-        int maxFreq=v.begin()->second;
         int ans=0;
-        for(auto it: v){
-            if(it.second==maxFreq)
-                ans+=it.second;
-            else
-                break;
+        for(int i=1; i<101; i++){
+            if(freq[i]==maxFreq)
+                ans+=maxFreq;
         }
         return ans;
     }
