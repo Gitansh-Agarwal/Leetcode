b7d0ce897b63945280951791bab14345b53a7602
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-03-09 00:16:21 +0530

    Time: 3 ms (84.14%), Space: 23.1 MB (5.04%) - LeetHub

---
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    static bool compare(const pair<int, int>& a, const pair<int, int>& b){
+        return a.second>b.second;
+    }
+    int maxFrequencyElements(vector<int>& nums) {
+        unordered_map<int, int> mpp;
+        for(auto it:nums){
+            mpp[it]++;
+        }
+        vector<pair<int, int>> v(mpp.begin(), mpp.end());
+        sort(v.begin(), v.end(), compare);
+        int maxFreq=(*(v.begin())).second;
+        int ans=0;
+        for(auto it: v){
+            if(it.second==maxFreq)
+                ans+=it.second;
+            else
+                break;
+        }
+        return ans;
+    }
+};
