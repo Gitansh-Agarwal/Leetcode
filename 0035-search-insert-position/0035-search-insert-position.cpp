406bbffa7f06bacadbc4aefca8235c5a796625d8
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-03-17 18:45:15 +0530

    Time: 0 ms (100.00%), Space: 11.9 MB (81.58%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
+        // int n=nums.size();
+        // int low=0;
+        // int high=n-1;
+        // while(low<=high){
+        //     int mid=(low+high)/2;
+        //     if(nums[mid] == target){
+        //         return mid;
+        //     }
+        //     else if(nums[mid] < target){
+        //         low=mid+1;
+        //     }
+        //     else{
+        //         high=mid-1;
+        //     }
+        // }
+        // return low;
+        
+        //We are simply asked to find out the lower bound of the given value.
+        //Correct Answer= Using Lower Bound
         int n=nums.size();
         int low=0;
         int high=n-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid] == target){
-                return mid;
-            }
-            else if(nums[mid] < target){
-                low=mid+1;
-            }
-            else{
+        int ans=n;
+        while(low <= high){
+            int mid=low+(high-low)/2;
+            if(nums[mid] >= target){
+                ans=mid;
                 high=mid-1;
             }
+            else{
+                low=mid+1;
+            }
         }
-        return low;
+        return ans;
     }
 };
