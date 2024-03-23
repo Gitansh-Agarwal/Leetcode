e5a44ca98e035cc2f57e463e8382b302e5f09ff6
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-03-23 21:28:33 +0530

    Time: 7 ms (16.38%), Space: 12.7 MB (20.77%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -8,10 +8,20 @@ public:
 
         while(low<=high){
             int mid=(low+high)/2;
+            
+            //search space is already sorted, then nums[low] will always be the smallest in that search space
+            if(nums[low]<=nums[high]){
+                ans=min(ans, nums[low]);
+                break;
+            }
+            
+            //otherwise either one of the left or right half is sorted.
+            //left half is sorted
             if(nums[low]<=nums[mid]){
                 ans=min(ans, nums[low]);
                 low=mid+1;
             }            
+            //right half is sorted
             else{
                 ans=min(ans, nums[mid]);
                 high=mid-1;
