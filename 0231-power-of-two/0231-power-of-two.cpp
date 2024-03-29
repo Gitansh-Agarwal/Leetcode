21df9e5beb0ed22c44f756fca870031810923242
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-03-29 17:52:24 +0530

    Time: 2 ms (37.53%), Space: 7.3 MB (35.87%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -23,36 +23,43 @@ public:
 //         }
 //         return false;
 
-        //Using Bit Manipulation
-        // if(n<0)
-        //     return false;
-        // int count=0;
-        // while(n!=0){
-        //     if(n & 1 == 1){
-        //         count++;
-        //     }
-        //     n = n >> 1;
-        // }
-        // if(count == 1)
-        //     return true;
-        // return false;
-        
-        
-        if(n<=0){
+        // Using Bit Manipulation
+        if(n<=0)//00000 is same as 0
             return false;
-        }
         int count=0;
-        for(int i=0; i<32; i++){
-            if((n>>i) & 1 == 1){
+        while(n!=0){
+            if(n & 1 == 1){
                 count++;
             }
+            n = n >> 1;
             if(count>1){
                 return false;
             }
         }
-        if(count==1){
+        if(count == 1)
             return true;
-        }
         return false;
+        
+        
+        //OR 
+        
+        //Another solution using Bit Manipulation.
+        
+        // if(n<=0){
+        //     return false;
+        // }
+        // int count=0;
+        // for(int i=0; i<32; i++){
+        //     if((n>>i) & 1 == 1){
+        //         count++;
+        //     }
+        //     if(count>1){
+        //         return false;
+        //     }
+        // }
+        // if(count==1){
+        //     return true;
+        // }
+        // return false;
     }
 };
