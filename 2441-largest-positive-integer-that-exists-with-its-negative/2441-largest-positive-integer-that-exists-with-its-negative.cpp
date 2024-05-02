37f71cbd66de94e25ea9fc30ccb2c80bc2f3dacb
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-05-03 01:35:20 +0530

    Time: 18 ms (61.51%), Space: 22.9 MB (65.72%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
@@ -23,17 +23,48 @@ public:
         //TC=O(NlogN+NlogN) = O(NlogN).
         //Sorting the array and then using binary search for each and every positive value starting from the right. 
         //Sorting
+//         sort(nums.begin(), nums.end());
+        
+//         //Applying binary search
+//         for(int i=nums.size()-1; i>0; i--){
+//             if(nums[i]<0){
+//                 break;
+//             }
+            
+//             int x=-nums[i];
+//             if(binary_search(nums.begin(), nums.begin()+i, x)){
+//                 return nums[i];
+//             }
+//         }
+//         return -1;
+        
+        
+        
+        //TC=O(NlogN + N) = O(NlogN).
+        //Sorting the array and then using two pointer approach.
+        //Sorting
         sort(nums.begin(), nums.end());
         
-        //Applying binary search
-        for(int i=nums.size()-1; i>0; i--){
-            if(nums[i]<0){
+        //Using two pointer approach.
+        int right=nums.size()-1;
+        int left=0;
+        while(left<right){
+            if(nums[right]<0)
                 break;
-            }
             
-            int x=-nums[i];
-            if(binary_search(nums.begin(), nums.begin()+i, x)){
-                return nums[i];
+            if(abs(nums[left]) < nums[right]){
+                right--;
+            }
+            else if(abs(nums[left]) > nums[right]){
+                left++;
+            }
+            //checking if the values have opposite signs
+            else if(-nums[left] == nums[right]){
+                return nums[right];
+            }
+            //it means that the values have same sign which means there is no largest positive integer k such that -k also exists in the array.
+            else{
+                break;
             }
         }
         return -1;
