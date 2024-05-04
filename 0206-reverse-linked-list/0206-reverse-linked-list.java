34a0cf13e6b5b50d64237917b6d003b1474cfe89
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-05-04 17:39:18 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (74.61%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -12,27 +12,27 @@ class Solution {
     public ListNode reverseList(ListNode head) {
         //TC=O(N), SC=O(1).
         //Using three Pointers
-        ListNode prev=null, curr=null, front=head;
-        while(front!=null){
-            prev=curr;
-            curr=front;
-            front=front.next;
-            curr.next=prev;
-        }
-        head=curr;
-        return head;
+        // ListNode prev=null, curr=null, front=head;
+        // while(front!=null){
+        //     prev=curr;
+        //     curr=front;
+        //     front=front.next;
+        //     curr.next=prev;
+        // }
+        // head=curr;
+        // return head;
         
         
         //TC=O(N), SC=O(N){recursion stack space.}
         //Using Recursion.
-//         if(head==nullptr || head->next==nullptr){
-//             return head;
-//         }
-//         ListNode* newHead = reverseList(head->next);
-//         ListNode* front = head->next;
-//         front->next=head;
-//         head->next=nullptr;
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        ListNode front = head.next;
+        front.next=head;
+        head.next=null;
         
-//         return newHead;
+        return newHead;
     }
 }
