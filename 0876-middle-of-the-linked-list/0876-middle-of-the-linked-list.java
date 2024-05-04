cceb019c69f63869c374043cb5af157c6e91c8ec
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-05-04 15:27:14 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (50.87%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -14,34 +14,34 @@ class Solution {
         //TC=O(N+N/2).
         
         //Finding the length of the linked list
-        int cnt=0;
-        ListNode temp=head;
-        while(temp!=null){
-            cnt++;
-            temp=temp.next;
-        }
+//         int cnt=0;
+//         ListNode temp=head;
+//         while(temp!=null){
+//             cnt++;
+//             temp=temp.next;
+//         }
         
-        //Finding the middle node based on the length of the linked list.
-        int middNode=(cnt/2)+1;
-        temp=head;
-        while(temp!=null){
-            middNode--;
-            if(middNode==0){
-                break;
-            }
-            temp=temp.next;
-        }
-        return temp;
+//         //Finding the middle node based on the length of the linked list.
+//         int middNode=(cnt/2)+1;
+//         temp=head;
+//         while(temp!=null){
+//             middNode--;
+//             if(middNode==0){
+//                 break;
+//             }
+//             temp=temp.next;
+//         }
+//         return temp;
         
         
         
         //Optimal Code
         //TC=O(N/2).
-        // Node slow=head, fast=head;
-        // while(fast!=null && fast.next!=null){
-        //     slow=slow.next;
-        //     fast=fast.next.next;
-        // }
-        // return slow.data;
+        ListNode slow=head, fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
     }
 }
