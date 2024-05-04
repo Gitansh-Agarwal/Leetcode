0df293484e0e6b4f2d168a96205510460d8d42f7
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-05-04 17:36:46 +0530

    Time: 0 ms (100.00%), Space: 11.8 MB (5.89%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,6 +11,8 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
+        //Brute force Solution.
+        //TC=O(2N), SC=O(N){stack space}.
         //  ListNode *p=head,*q=nullptr,*r=nullptr;
         // while(p!=nullptr){
         //     r=q;
@@ -22,15 +24,30 @@ public:
         // return head;
         
         
+        //TC=O(N), SC=O(1).
+        //Using three Pointers
+        // ListNode *prev=nullptr, *curr=nullptr, *front=head;
+        // while(front!=nullptr){
+        //     prev=curr;
+        //     curr=front;
+        //     front=front->next;
+        //     curr->next=prev;
+        // }
+        // head=curr;
+        // return head;
         
-        ListNode *prev=nullptr, *curr=nullptr, *front=head;
-        while(front!=nullptr){
-            prev=curr;
-            curr=front;
-            front=front->next;
-            curr->next=prev;
+        
+        //TC=O(N), SC=O(N){recursion stack space.}
+        //Using Recursion.
+        if(head==nullptr || head->next==nullptr){
+            return head;
         }
-        head=curr;
-        return head;
+        ListNode* newHead = reverseList(head->next);
+        ListNode* front = head->next;
+        front->next=head;
+        head->next=nullptr;
+        
+        return newHead;
+        
     }
 };
