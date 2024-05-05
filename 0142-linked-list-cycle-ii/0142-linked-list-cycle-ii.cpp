3b7ef4c3394689bb509543ebcca0e06f95e290a9
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-05-05 18:27:05 +0530

    Time: 6 ms (69.38%), Space: 9.9 MB (96.36%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,20 +9,43 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        unordered_set<ListNode*> st;
+        //TC=O(N*2*logN) or (N*2*1)
+        //SC=O(N).
+//         unordered_set<ListNode*> st;
         
-        if(head==nullptr || head->next==nullptr){
-            return nullptr;
-        }
+//         if(head==nullptr || head->next==nullptr){
+//             return nullptr;
+//         }
         
-        ListNode *temp=head;
-        while(temp!=nullptr){
-            if(st.find(temp)!=st.end()){
-                break;
+//         ListNode *temp=head;
+//         while(temp!=nullptr){
+//             if(st.find(temp)!=st.end()){
+//                 break;
+//             }
+//             st.insert(temp);
+//             temp=temp->next;
+//         }
+//         return temp;
+        
+        
+        
+        //Optimal Method
+        //TC=O(N).
+        //SC=O(1).
+        ListNode *slow=head, *fast=head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+            
+            if(slow==fast){
+                slow=head;
+                while(slow!=fast){
+                    slow=slow->next;
+                    fast=fast->next;
+                }
+                return slow;//Both fast and slow are standing at the starting point of loop in the linked list.
             }
-            st.insert(temp);
-            temp=temp->next;
         }
-        return temp;
+        return nullptr;
     }
 };
