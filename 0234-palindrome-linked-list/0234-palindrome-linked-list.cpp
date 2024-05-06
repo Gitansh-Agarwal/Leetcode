72845fc73c5a3a9c985fa201e064ce3c67f96b7a
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-05-06 22:05:59 +0530

    Time: 173 ms (50.39%), Space: 130.8 MB (10.35%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -11,53 +11,74 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        ListNode *slow=head, *fast=head;
-        while(fast!=nullptr && fast->next!=nullptr){
-            slow=slow->next;
-            fast=fast->next->next;
-        }
-        ListNode *prev=nullptr, *curr=nullptr, *front;
-        if(fast==nullptr){
-            front=slow;
-            ListNode* backHead;
-            while(front!=nullptr){
-                prev=curr;
-                curr=front;
-                front=front->next;
-                curr->next=prev;
-            }
-            backHead=curr;
-            ListNode* temp1=head;
-            ListNode* temp2=backHead;
-            while(temp2!=nullptr){
-                if(temp1->val!=temp2->val){
-                    return false;
-                }
-                temp1=temp1->next;
-                temp2=temp2->next;
-            }
+        //Inefficient Solution.
+//         ListNode *slow=head, *fast=head;
+//         while(fast!=nullptr && fast->next!=nullptr){
+//             slow=slow->next;
+//             fast=fast->next->next;
+//         }
+//         ListNode *prev=nullptr, *curr=nullptr, *front;
+//         if(fast==nullptr){
+//             front=slow;
+//             ListNode* backHead;
+//             while(front!=nullptr){
+//                 prev=curr;
+//                 curr=front;
+//                 front=front->next;
+//                 curr->next=prev;
+//             }
+//             backHead=curr;
+//             ListNode* temp1=head;
+//             ListNode* temp2=backHead;
+//             while(temp2!=nullptr){
+//                 if(temp1->val!=temp2->val){
+//                     return false;
+//                 }
+//                 temp1=temp1->next;
+//                 temp2=temp2->next;
+//             }
             
-        }//return false;
-        //fast->next==nullptr
-        else{
-            prev=nullptr, curr=nullptr, front=slow->next;
-            ListNode *backHead;
-            while(front!=nullptr){
-                prev=curr;
-                curr=front;
-                front=front->next;
-                curr->next=prev;
+//         }//return false;
+//         //fast->next==nullptr
+//         else{
+//             prev=nullptr, curr=nullptr, front=slow->next;
+//             ListNode *backHead;
+//             while(front!=nullptr){
+//                 prev=curr;
+//                 curr=front;
+//                 front=front->next;
+//                 curr->next=prev;
+//             }
+//             backHead=curr;
+//             ListNode *temp1=head;
+//             ListNode *temp2=backHead;
+//             while(temp2!=nullptr){
+//                 if(temp1->val!=temp2->val){
+//                     return false;
+//                 }
+//                 temp1=temp1->next;
+//                 temp2=temp2->next;
+//             }
+//         }
+//         return true;
+        
+        
+        
+        //IInd Method 
+        //Copy all the elements in an array and then check for palindrome.
+        vector<int> temp;
+        ListNode* temp1=head;
+        while(temp1!=nullptr){
+            temp.push_back(temp1->val);
+            temp1=temp1->next;
+        }
+        int left=0, right=temp.size()-1;
+        while(left < right){
+            if(temp[left] != temp[right]){
+                return false;
             }
-            backHead=curr;
-            ListNode *temp1=head;
-            ListNode *temp2=backHead;
-            while(temp2!=nullptr){
-                if(temp1->val!=temp2->val){
-                    return false;
-                }
-                temp1=temp1->next;
-                temp2=temp2->next;
-            }
+            left++;
+            right--;
         }
         return true;
     }
