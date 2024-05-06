d10c0b95d8bcae840eaf8af8c024920a09dc73ab
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-05-07 01:24:57 +0530

    Time: 191 ms (16.91%), Space: 120.5 MB (51.43%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,8 +10,40 @@
  */
 class Solution {
 public:
+    ListNode* reverse(ListNode* head){
+        ListNode *prev=nullptr, *curr=nullptr, *front=head; 
+        while(front!=nullptr){
+            prev=curr;
+            curr=front;
+            front=front->next;
+            curr->next=prev;
+        }
+        head=curr;
+        
+        return head;
+    }
     bool isPalindrome(ListNode* head) {
-        //Inefficient Solution.
+        //Ist Method 
+        //Copy all the elements in an array and then check for palindrome.
+        // vector<int> temp;
+        // ListNode* temp1=head;
+        // while(temp1!=nullptr){
+        //     temp.push_back(temp1->val);
+        //     temp1=temp1->next;
+        // }
+        // int left=0, right=temp.size()-1;
+        // while(left < right){
+        //     if(temp[left] != temp[right]){
+        //         return false;
+        //     }
+        //     left++;
+        //     right--;
+        // }
+        // return true;
+        
+        
+        
+        //Optimal Solution but not done efficiently.
 //         ListNode *slow=head, *fast=head;
 //         while(fast!=nullptr && fast->next!=nullptr){
 //             slow=slow->next;
@@ -63,23 +95,26 @@ public:
 //         return true;
         
         
-        
-        //IInd Method 
-        //Copy all the elements in an array and then check for palindrome.
-        vector<int> temp;
-        ListNode* temp1=head;
-        while(temp1!=nullptr){
-            temp.push_back(temp1->val);
-            temp1=temp1->next;
+        // Refined Version of Optimal Solution
+        ListNode *slow=head, *fast=head;
+        //Here slow will point to middle node m1 and not on middle node m2.
+        while(fast->next!=nullptr && fast->next->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        int left=0, right=temp.size()-1;
-        while(left < right){
-            if(temp[left] != temp[right]){
+        ListNode *newHead=reverse(slow->next);
+        ListNode *first=head, *second=newHead;
+        while(second!=nullptr){
+            if(first->val!=second->val){
+                //restoring the original linked list.
+                reverse(newHead);
                 return false;
             }
-            left++;
-            right--;
+            first=first->next;
+            second=second->next;
         }
+        reverse(newHead);
         return true;
+        
     }
 };
