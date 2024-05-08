7fe253f2ad4e0ceab0c007f215c8ec89a4f829b7
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-05-08 18:30:56 +0530

    Time: 92 ms (99.22%), Space: 54.4 MB (92.53%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,22 +10,92 @@
  */
 class Solution {
 public:
+    ListNode* middNode(ListNode* head){
+        ListNode *slow=head, *fast=head->next;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+    //OR Midlle node m1(even length LL) or simply middle node in LL can also be find out using the below method.
+    // ListNode* middNode(ListNode* head){
+    //     ListNode *slow=head, *fast=head;
+    //     while(fast!=nullptr || fast->next!=nullptr || fast->next->next!=nullptr){
+    //         slow=slow->next;
+    //         fast=fast->next->next;
+    //     }
+    //     return slow;
+    // }
+    
+    
+    ListNode* merge2SortedLL(ListNode* leftHead, ListNode* rightHead){
+        ListNode *mergeHead=nullptr, *temp=nullptr;
+        while(leftHead!=nullptr && rightHead!=nullptr){
+            if(leftHead->val < rightHead->val){
+                if(mergeHead==nullptr){
+                    mergeHead=leftHead;
+                    temp=leftHead;
+                }
+                else{
+                    temp->next=leftHead;
+                    temp=temp->next;
+                }
+                leftHead=leftHead->next;
+            }
+            else{
+                if(mergeHead==nullptr){
+                    mergeHead=rightHead;
+                    temp=rightHead;
+                }
+                else{
+                    temp->next=rightHead;
+                    temp=temp->next;
+                }
+                rightHead=rightHead->next;
+            }
+        }
+        if(leftHead==nullptr){
+            temp->next=rightHead;
+        }
+        else{
+            temp->next=leftHead;
+        }
+        return mergeHead;
+    }
     ListNode* sortList(ListNode* head) {
-        ListNode* temp=head;
-        vector<int> tempVec;
-        while(temp!=nullptr){
-            tempVec.push_back(temp->val);
-            temp=temp->next;
+//         ListNode* temp=head;
+//         vector<int> tempVec;
+//         while(temp!=nullptr){
+//             tempVec.push_back(temp->val);
+//             temp=temp->next;
+//         }
+        
+//         sort(tempVec.begin(), tempVec.end());
+        
+//         temp=head;
+//         int i=0;
+//         while(temp!=nullptr){
+//             temp->val=tempVec[i++];
+//             temp=temp->next;
+//         }
+//         return head;
+        
+        
+        
+        //Method 2
+        if(head==nullptr || head->next==nullptr){
+            return head;
         }
         
-        sort(tempVec.begin(), tempVec.end());
+        ListNode *middleNode = middNode(head);
+        ListNode *rightHead=middleNode->next;
+        middleNode->next=nullptr;
+        ListNode *leftHead=head;
         
-        temp=head;
-        int i=0;
-        while(temp!=nullptr){
-            temp->val=tempVec[i++];
-            temp=temp->next;
-        }
-        return head;
+        leftHead = sortList(leftHead);
+        rightHead = sortList(rightHead);
+        
+        return merge2SortedLL(leftHead, rightHead);
     }
 };
