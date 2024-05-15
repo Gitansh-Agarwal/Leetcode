0b4b3416a4bbe9021ab2f2184b30c9f44f9c9253
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-05-15 21:01:16 +0530

    Time: 3 ms (47.69%), Space: 7.2 MB (98.24%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,10 +1,32 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        // int index=-1;
-        // //Finding out the position of the first character of string s in string goal.
-        // for(int i=0; i<)
+        //Assuming that both the strings s and t are of equal length.
         
+        //The below method is not working fine.
+//         int index=-1;
+//         sort(s.begin(),s.end());
+//         int lengthOfS=s.length();
+//         //Finding out the position of the first character of string s in string goal.
+//         for(int i=0; i<lengthOfS; i++){
+//             if(goal[i]==s[0]){
+//                 index=i;
+//                 break;
+//             }
+//         }
+//         for(int i=1; i<lengthOfS; i++){
+//             char temp=goal[(index+i)%lengthOfS];
+            
+//             if(s[i]!=temp){
+//                 return false;
+//             }
+//         }
+//         return true;
+        
+        
+        
+        
+        //This Below Method is working fine.
         int lengthOfS=s.length();
         for(int i=0; i<lengthOfS; i++){
             char temp=s[0];
