2d10926d5dc8be6ea17d4c2840b42583674e08b6
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-07-16 19:18:21 +0530

    Time: 4 ms (41.25%), Space: 9.5 MB (14.01%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int findNoOfSplits(vector<int>&nums, int sum){
+        int noOfSplits=1;
+        int total=0;
+        
+        for(int i=0; i<nums.size(); i++){
+            if(total+nums[i] <= sum){
+                total+=nums[i];
+            }
+            else{
+                noOfSplits++;
+                total=nums[i];
+            }
+        }
+        return noOfSplits;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int n=nums.size();
+        //below if statement not required.
+        if(k>n){
+            return -1;
+        }
+        
+        
+        int low=*max_element(nums.begin(), nums.end());
+        int high=accumulate(nums.begin(), nums.end(), 0);
+        int ans=-1;
+        
+        while(low<=high){
+            int mid = (low+high)/2;
+            int splits = findNoOfSplits(nums, mid);
+            if(splits == k){
+                ans=mid;
+                high=mid-1;
+            }
+            else if(splits > k){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        // return ans;
+         return low;
+    }
+};
