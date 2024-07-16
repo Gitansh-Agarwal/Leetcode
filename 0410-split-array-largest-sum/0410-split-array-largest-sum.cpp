1e34ca542634d1112db09b80c2768e90aa4a5909
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-07-16 19:40:15 +0530

    Time: 4 ms (41.25%), Space: 9.5 MB (14.01%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -30,26 +30,26 @@ public:
         while(low<=high){
             int mid = (low+high)/2;
             int splits = findNoOfSplits(nums, mid);
-            // if(splits == k){
-            //     ans=mid;
-            //     high=mid-1;
-            // }
-            // else if(splits > k){
-            //     low=mid+1;
-            // }
-            // else{
-            //     high=mid-1;
-            // }
-            
-            //OR
-            if(splits > k){
+            if(splits == k){
+                ans=mid;
+                high=mid-1;
+            }
+            else if(splits > k){
                 low=mid+1;
             }
             else{
                 high=mid-1;
             }
+            
+            //OR
+            // if(splits > k){
+            //     low=mid+1;
+            // }
+            // else{
+            //     high=mid-1;
+            // }
         }
-        // return ans;
+        // return ans;//This is not working dont know
          return low;
     }
 };
