f4b3035cd63bf1c367986b9ddb1957ef76b141c8
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-07-21 00:46:17 +0530

    Time: 26 ms (52.59%), Space: 94.8 MB (47.90%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,33 +4,80 @@ public:
         //Brute Force Solution = Merging two sorted arrays and then finding out the median of the resultant. 
         //TC=O(m+n).
         //SC=O(1).
+//         int n1=nums1.size();
+//         int n2=nums2.size();
+//         vector<int> mergedVector(n1+n2);
+        
+//         int i=0, j=0;
+//         int k=0;
+//         while(i<n1 && j<n2){
+//             if(nums1[i] < nums2[j]){
+//                 mergedVector[k++]=nums1[i++];
+//             }
+//             else{
+//                 mergedVector[k++]=nums2[j++];
+//             }
+//         }
+//         while(i<n1){
+//             mergedVector[k++]=nums1[i++];
+//         }
+//         while(j<n2){
+//             mergedVector[k++]=nums2[j++];
+//         }
+        
+//         int n=n1+n2;
+//         if(n%2==1){
+//             return mergedVector[n/2];
+//         }
+//         else{
+//             return (double)(mergedVector[n/2]+mergedVector[n/2-1])/2.0;
+//         }
+        
+        
+        
+        
+        
+        //Better Solution
+        //
         int n1=nums1.size();
         int n2=nums2.size();
-        vector<int> mergedVector(n1+n2);
-        
+        int n=n1+n2;
+        int ind1=n/2;
+        int ind2=ind1-1;
+        int ind1El=-1, ind2El=-1;
+        int cnt=0;
         int i=0, j=0;
-        int k=0;
         while(i<n1 && j<n2){
             if(nums1[i] < nums2[j]){
-                mergedVector[k++]=nums1[i++];
+                if(cnt == ind1) ind1El=nums1[i];
+                if(cnt == ind2) ind2El=nums1[i];
+                i++;
             }
             else{
-                mergedVector[k++]=nums2[j++];
+                if(cnt == ind1) ind1El=nums2[j];
+                if(cnt == ind2) ind2El=nums2[j];
+                j++;
             }
+            cnt++;
         }
         while(i<n1){
-            mergedVector[k++]=nums1[i++];
+            if(cnt == ind1) ind1El=nums1[i];
+            if(cnt == ind2) ind2El=nums1[i];
+            i++;
+            cnt++;
         }
         while(j<n2){
-            mergedVector[k++]=nums2[j++];
+            if(cnt == ind1) ind1El=nums2[j];
+            if(cnt == ind2) ind2El=nums2[j];
+            j++;
+            cnt++;
         }
         
-        int n=n1+n2;
-        if(n%2==1){
-            return mergedVector[n/2];
+        if(n%2 == 1){
+            return ind1El;
         }
         else{
-            return (double)(mergedVector[n/2]+mergedVector[n/2-1])/2.0;
+            return (double)(ind1El+ind2El)/2.0;
         }
     }
 };
