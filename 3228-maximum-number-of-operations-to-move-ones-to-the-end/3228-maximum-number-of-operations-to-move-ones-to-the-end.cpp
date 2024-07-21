16506f0444ab4caaa7b45911b89024b5c53eb5d1
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-07-21 15:44:58 +0530

    Time: 19 ms (100.00%), Space: 14.3 MB (33.33%) - LeetHub

--- 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -2,40 +2,61 @@ class Solution {
 public:
     int maxOperations(string s) {
         //Complex Version
-        int n=s.size();
-        int startIndex=-1;
-        //traversing from the end till we find a '0' as after that zero the "1's" will start to have an impact while shifting.
-        for(int i=n-1; i>=0; i--){
-            if(s[i]=='0'){
-                startIndex=i;
-                break;
-            }
-        }
-        // if(startIndex==n){
-        //     return 0;
+        // int n=s.size();
+        // int startIndex=-1;
+        // //traversing from the end till we find a '0' as after that zero the "1's" will start to have an impact while shifting.
+        // for(int i=n-1; i>=0; i--){
+        //     if(s[i]=='0'){
+        //         startIndex=i;
+        //         break;
+        //     }
         // }
-        int ans=0;
-        int impact=1;
-        for(int i=startIndex; i>=0; i--){
-            if(s[i]=='0')   continue;
-            int cnt=1;
-            int j;
-            for(j=i-1; j>=0; j--){
-                if(s[j]=='0')   break;
-                cnt++;
-            }
-            ans+=cnt*impact;
-            impact++;
-            i=j+1;
-        }
-        return ans;
+        // int ans=0;
+        // int impact=1;
+        // for(int i=startIndex; i>=0; i--){
+        //     if(s[i]=='0')   continue;
+        //     int cnt=1;
+        //     int j;
+        //     for(j=i-1; j>=0; j--){
+        //         if(s[j]=='0')   break;
+        //         cnt++;
+        //     }
+        //     ans+=cnt*impact;
+        //     impact++;
+        //     i=j+1;
+        // }
+        // return ans;
         
         
         
         
         //Simplified Version.
-        // reverse(s.begin(),s.end());
-        // int startIndex=n;
+        reverse(s.begin(),s.end());
         
+        int n=s.size();
+        int startIndex=n;
+        
+        for(int i=0; i<n; i++){
+            if(s[i]=='0'){
+                startIndex=i;
+                break;
+            }
+        }
+        
+        int ans=0;
+        int impact=1;
+        for(int i=startIndex; i<n; i++){
+            if(s[i]=='0')   continue;
+            int cnt=1;
+            int j;
+            for(j=i+1; j<n; j++){
+                if(s[j]=='0')   break;
+                cnt++;
+            }
+            ans+=cnt*impact;
+            impact++;
+            i=j-1;
+        }
+        return ans;
     }
 };
