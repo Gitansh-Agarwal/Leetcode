fc5451cafba10f30de04f06c8316a0e2047704a7
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-07-21 15:32:23 +0530

    Time: 29 ms (66.67%), Space: 14.3 MB (33.33%) - LeetHub

--- 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int maxOperations(string s) {
+        //Complex Version
         int n=s.size();
-        int startIndex=n;
+        int startIndex=-1;
         //traversing from the end till we find a '0' as after that zero the "1's" will start to have an impact while shifting.
         for(int i=n-1; i>=0; i--){
             if(s[i]=='0'){
@@ -10,9 +11,9 @@ public:
                 break;
             }
         }
-        if(startIndex==n){
-            return 0;
-        }
+        // if(startIndex==n){
+        //     return 0;
+        // }
         int ans=0;
         int impact=1;
         for(int i=startIndex; i>=0; i--){
@@ -28,5 +29,13 @@ public:
             i=j+1;
         }
         return ans;
+        
+        
+        
+        
+        //Simplified Version.
+        // reverse(s.begin(),s.end());
+        // int startIndex=n;
+        
     }
 };
