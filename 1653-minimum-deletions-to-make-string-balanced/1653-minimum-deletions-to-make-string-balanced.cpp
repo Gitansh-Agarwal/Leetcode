f63d56aab5381e1f06e2229a02c8875808aaaac6
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-07-31 20:34:45 +0530

    Time: 88 ms (64.01%), Space: 23.7 MB (73.71%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
+        //TC=O(N), SC=O(N).
 //         int count=0;
 //         stack<char> stk;
         
@@ -18,29 +19,52 @@ public:
         
         
         //using two vectors for count.
+        //TC=O(3N), SC=O(2N)
+//         int n=s.size();
+//         vector<int> left_b(n,0);
+//         vector<int> right_a(n,0);
+        
+//         int countb=0;
+//         for(int i=0; i<n; i++){
+//             left_b[i]=countb;
+//             if(s[i]=='b'){
+//                 countb++;
+//             }
+//         }
+        
+//         int counta=0;
+//         for(int i=n-1; i>=0; i--){
+//             right_a[i]=counta;
+//             if(s[i]=='a'){
+//                 counta++;
+//             }
+//         }
+        
+//         int count=INT_MAX;
+//         for(int i=0; i<n; i++){
+//             count=min(count, left_b[i]+right_a[i]);
+//         }
+//         return count;
+        
+        //TC=O(2N), SC=O(1).
         int n=s.size();
-        vector<int> left_b(n,0);
-        vector<int> right_a(n,0);
-        
-        int countb=0;
-        for(int i=0; i<n; i++){
-            left_b[i]=countb;
-            if(s[i]=='b'){
-                countb++;
-            }
-        }
-        
         int counta=0;
-        for(int i=n-1; i>=0; i--){
-            right_a[i]=counta;
+        for(int i=0; i<n; i++){
             if(s[i]=='a'){
                 counta++;
             }
         }
-        
+        int countb=0;
         int count=INT_MAX;
+        
         for(int i=0; i<n; i++){
-            count=min(count, left_b[i]+right_a[i]);
+            if(s[i]=='a'){
+                counta--;
+            }
+            count=min(count, counta+countb);
+            if(s[i]=='b'){
+                countb++;
+            }
         }
         return count;
     }
