7070d98acaeff2d158f0759e84fbaedbd3e070e1
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-08-25 02:49:28 +0530

    Time: 15 ms (80.51%), Space: 16.2 MB (78.64%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -23,8 +23,55 @@ class Solution {
             }
             return true;
         }
+    
+    
+    bool dfs(int start, int colorVal, vector<vector<int>>&graph, int color[]){
+            color[start]=colorVal;
+                
+            for(auto it: graph[start]){
+                //if the adjacent node is not yet coloured, we colour it with the opposite color to that of the (node).
+                if(color[it] == -1){
+
+                    if(dfs(it, !color[start], graph, color) == false){
+                        return false;
+                    }
+
+                }
+                // the neighbour is already coloured, then check is the adjacent guy have the same colour, if yes, then someone must have coloured it on some other path. 
+                else if(color[it] == color[start]){
+                    return false;
+                }
+            }
+            
+            return true;
+        }
 public:
     bool isBipartite(vector<vector<int>>& graph) {
+        //Using BFS Traversal.
+        //TC=O(N*M){BFS Traversal} + O(V){for connected components}.
+        //SC=O(N*M).{queue space} + O(V){color array space}.
+//         int V=graph.size();
+//         int color[V];
+//         for(int i=0; i<V; i++){
+//             color[i]=-1;
+//         }
+        
+//         //for connected components.
+//         for(int i=0; i<V; i++){
+//             if(color[i] == -1){
+//                 if(bfs(i, graph, color) == false){
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+        
+        
+        
+        
+        //Using DFS Traversal.
+        //TC=O(N*M){BFS Traversal} + O(V){for connected components}.
+        //SC=O(N*M).{Recursive Stack Space} + O(V){color array space}.
         int V=graph.size();
         int color[V];
         for(int i=0; i<V; i++){
@@ -34,7 +81,7 @@ public:
         //for connected components.
         for(int i=0; i<V; i++){
             if(color[i] == -1){
-                if(bfs(i, graph, color) == false){
+                if(dfs(i, 0, graph, color) == false){
                     return false;
                 }
             }
