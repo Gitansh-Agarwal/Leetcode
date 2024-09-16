6ba00060608bd931746a1f3816e16c499349a7f3
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-09-17 02:34:25 +0530

    Time: 15 ms (68.10%), Space: 17.4 MB (72.14%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 private:
     bool dfsCheck(int node, vector<int> adj[], vector<int>& vis, vector<int>& pathVis, 
-    stack<int>& st){
+    vector<int>& orderOfCourses){
         
         vis[node]=1;
         pathVis[node]=1;
@@ -10,7 +10,7 @@ private:
         for(auto it: adj[node]){
             //when the given node is unvisited, do a dfs call on it to visit it.
             if(!vis[it]){
-                if(dfsCheck(it, adj, vis, pathVis, st) == true){
+                if(dfsCheck(it, adj, vis, pathVis, orderOfCourses) == true){
                     return true;
                 }
             }
@@ -20,35 +20,63 @@ private:
             }
         }
         pathVis[node]=0;
-        st.push(node);
+        orderOfCourses.push_back(node);
         return false;
     }
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        //Using Variation of TopoSort Method.
         //TC=O(V+E){as the given graph is a directed graph and we are doing a normal dfs check}.
         //SC=O(2*V).
+        
         vector<int> adj[numCourses];
         int n=prerequisites.size();
         for(int i=0; i<n; i++){
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
         }
 
         
         vector<int> vis(numCourses, 0);
         vector<int> pathVis(numCourses, 0);
-        stack<int> st;
         vector<int> orderOfCourses;
         for(int i=0; i<numCourses; i++){
             if(!vis[i]){
                 // if the dfs calls returns a true means that there exists a cycle in the graph.
-                if (dfsCheck(i, adj, vis, pathVis, st) == true)
+                if (dfsCheck(i, adj, vis, pathVis, orderOfCourses) == true)
                     return {}; // it is not possible to finish all courses as cycle exists in the graph.
             }
         }
-        while(!st.empty()){
-            orderOfCourses.push_back(st.top());
-            st.pop();
-        }
+       
         return orderOfCourses;// it is possible to finish all the courses as no cycle exists in the graph.
+        
+        
+        
+        
+        //Using Purely TopoSort Method.
+//         //TC=O(V+E){as the given graph is a directed graph and we are doing a normal dfs check}.
+//         //SC=O(2*V).
+//         vector<int> adj[numCourses];
+//         int n=prerequisites.size();
+//         for(int i=0; i<n; i++){
+//             adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+//         }
+
+        
+//         vector<int> vis(numCourses, 0);
+//         vector<int> pathVis(numCourses, 0);
+//         stack<int> st;
+//         vector<int> orderOfCourses;
+//         for(int i=0; i<numCourses; i++){
+//             if(!vis[i]){
+//                 // if the dfs calls returns a true means that there exists a cycle in the graph.
+//                 if (dfsCheck(i, adj, vis, pathVis, st) == true)
+//                     return {}; // it is not possible to finish all courses as cycle exists in the graph.
+//             }
+//         }
+//         while(!st.empty()){
+//             orderOfCourses.push_back(st.top());
+//             st.pop();
+//         }
+//         return orderOfCourses;// it is possible to finish all the courses as no cycle exists in the graph.
     }
 };
