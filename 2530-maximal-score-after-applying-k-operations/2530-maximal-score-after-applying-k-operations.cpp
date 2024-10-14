f0fdf6bb8d55018c0824f999ea635a95d2223442
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-10-14 13:37:49 +0530

    Time: 192 ms (66.58%), Space: 76 MB (79.55%) - LeetHub

--- 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.cpp
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.cpp
@@ -3,7 +3,9 @@ public:
     long long maxKelements(vector<int>& nums, int k) {
         long long int score=0;
         int n=nums.size();
-        priority_queue<int> pq(nums.begin(), nums.end());
+        // priority_queue<int> pq(std::less<int>(), nums);
+        //OR priority_queue<int> pq(nums.begin(), nums.end());
+        priority_queue<int> pq(begin(nums), end(nums));
         // while(!pq.empty()){
         //     cout<<pq.top()<<endl;
         //     pq.pop();
